6dea44f1377a6874577d9026fe862841750a1e58
James Essex <JMEssex@gmail.com>
2018-08-30 22:30:13 -0500

    update back to web3@1.0.0-beta.35

--- Inbox/test/inbox.test.js
+++ Inbox/test/inbox.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require ('web3');
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile.js')
+const { interface, bytecode } = require('../compile.js');
 
 require('events').EventEmitter.defaultMaxListeners = 0;
 
@@ -16,7 +16,7 @@ beforeEach(async () => {
   // Use one of those accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
-    .send({ from: accounts[0], gas: '1000000' })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
